4355d8fc4031349302cac457c7483552638c79b0
Lush Sleutsky <lushsleutsky@gmail.com>
2016-03-20 23:21:34 -0400

    adding website files for threebrotherswaterproofing.com

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3 Brothers Waterproofing
+
+A responsive website for a family owned basement waterproofing and home remodeling company that has been serving the Tri-State area since 1967
---
+++ quick_contact.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <title>3 Brothers Waterproofing Solutions Serving PA, NJ and Delaware</title>
+  <meta name="title" content="3 Brothers Waterproofing - Basement Waterproofing | Mold Remediation | Remodeling">
+  <meta charset="utf-8">
+  <meta name="robots" content="index, follow">
+  <meta name="robots" content="ALL">
+  <meta name="robots" content="INDEX, FOLLOW">
+  <meta name="robots" content="selection">
+  <meta name="location" content="Bucks County Pennsylvania">
+  <meta name="location" content="Montgomery County Pennsylvania">
+  <meta name="location" content="Chester County Pennsylvania">
+  <meta name="location" content="Delaware County Pennsylvania">
+  <meta name="location" content="Philadelphia Pennsylvania">
+  <meta name="language" content="English">
+  <meta name="profession" content="Basement Waterproofing">
+  <meta name="copyright" content="3 Brothers Waterproofing">
+  <meta name="revisit-after" content="7 days">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+
+  <meta name="document-classification" content="Basement Waterproofing and Mold Remediation">
+  <meta name="document-rating" content="General">
+  <meta name="document-state" content="Dynamic">
+  <meta name="Abstract" content="Basement Waterproofing and Mold Remediation">
+
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="http://www.threebrotherswaterproofing.com">
+  <meta property="og:description" content="3 Brothers Waterproofing provides award winning basement and crawlspace waterproofing, mold remediation, and remodeling services">
+
+  <meta name="description" content="Basement Waterproofing, Mold Remediation, Home Remodeling in PA, NJ, DE">
+  <meta name="description" content="3 Brothers Waterproofing Solutions waterproofing and wet basement contractor company.  Basement Systems offers award winning products and services. Free Leak Repair and Mold Removal Estimates.">
+  <meta name="keywords" content="Basement Waterproofing, Wet Basement, Waterproofing Contractor, Crawl Space Encapsulation, Sump Pump Systems, Waterproofing Products, Basement Systems, Leaky, Wet, Leaking, Flooded, Leaks, Waterpoof, Dry, Waterproofed">
+  <meta name="keywords" content="waterproofing, mold remediation, sump pumps, french drain, water damage, water penetration, water drainage">
+
+  <meta name="description" content="text">
+  <meta name="description" content="3 Brothers Waterproofing">
+  <meta name="description" content="3 Brothers Waterproofing - Waterproofing, Mold Remediation, Remodeling">
+  <meta name="description" content="3 Brothers Waterproofing provides waterproofing and mold remediation services for Philadelphia, New Jersey, and Delaware">
+  <meta name="description" content="3 Brothers Waterproofing provides waterproofing and mold remediation in Philadelphia, Bucks County, Montgomery County, Chester County, and Delaware County">
+  <meta name="description" content="3 Brothers Waterproofing provides waterproofing and mold remediation in Mercer County, Burlington County, Camden County, Gloucester County, Atlantic County, Cumberland County, Monmouth County, Middlesex County, Hunterdon County, Somerset County, Ocean County">
+  <meta name="description" content="3 Brothers Waterproofing provides waterproofing and mold remediation in Wilmington, Delaware and New Castle County Delaware">
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+  <link rel="shortcut icon" href="images/favicon.ico"  type="image/x-icon">
+  <link href='http://fonts.googleapis.com/css?family=Droid+Serif|Ubuntu' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="js/flexslider/flexslider.css">
+  <link rel="stylesheet" href="css/basic-style.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <link rel="stylesheet" href="css/custom_new.css">
+  <link href='http://fonts.googleapis.com/css?family=Permanent+Marker|Open+Sans:300italic,400italic,600italic,700italic,800italic,400,600,700,800,300|Roboto:400,100,300,100italic,400italic,300italic,500italic,500,700,700italic,900,900italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Droid+Sans|Droid+Sans+Mono' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+  <!-- JS-->
+  <script src="js/libs/modernizr-2.6.2.min.js"></script>
+  <!-- end JS-->
+</head>
+
+<body id="home" class="home">
+
+<header class="wrapper clearfix">  
+        <div id="banner">        
+          <div id="logo"><a href="index.html"><img src="images/logo.png" alt="3 Brothers Waterproofing Logo"></a></div>
+          <p id="logo_phone">1-844-671-4300</p>
+        </div>
+        
+        <!-- main navigation -->
+        <nav id="topnav" role="navigation">
+        <div class="menu-toggle">Menu</div>  
+          <ul class="srt-menu" id="menu-main-navigation">
+                <li><a href="waterproofing.html">Waterproofing</a>
+                     <ul>
+                        <li><a href="learning.html">Learning Center</a></li>
+                        <li><a href="outside.html">Outside Drainage</a></li>
+                        <li><a href="cracks.html">Crack Repair</a></li>
+                        <li><a href="basement-faq.html">Basement FAQs</a></li>
+                        <li><a href="resources.html#wp_resources">Resources</a></li>
+                        <li><a href="wp_gallery.html">Gallery</a></li>
+                    </ul> 
+                </li>
+                <li><a href="mold.html">Mold Removal</a>
+                 <ul>
+                  <li><a href="sealing.html">Sealing & Painting</a></li>
+                  <li><a href="mold-faq.html">Mold FAQs</a></li>
+                  <li><a href="resources.html#mold_resources">Resources</a></li>
+                  <li><a href="mold_gallery.html">Gallery</a></li>
+                </ul> 
+                </li>
+          <li><a href="remodeling.html">Remodeling</a>
+            <ul>
+                        <li><a href="resources.html#remod_resources">Resources</a></li>
+                        <li><a href="remod_gallery.html">Gallery</a></li>
+                    </ul> 
+          </li>
+          <li>
+            <a href="plumbing.html">Plumbing</a>
+            <ul>
+                    <li><a href="resources.html#plumbing_resources">Resources</a></li>
+                </ul> 
+          </li>
+          <li>
+            <a href="contact.html">Contact Us</a>
+              <ul>
+                <li><a href="about.html">About</a></li>
+                <li><a href="what-we-do.html">What We Do</a></li>
+                <li><a href="how-we-do.html">How We Do It</a></li>
+                <li><a href="affiliations.html">Affiliations</a></li>
+                <li><a href="coupon.html">Coupon</a></li>
+              </ul>
+          </li>
+    </ul>     
+    </nav><!-- #topnav -->
+</header><!-- end header -->
+<div id="slogan">
+          Family Owned - Serving The Tri-State Area Since 1967!
+        </div>
+ 
+<section id="page-header" class="clearfix">    
+  <div class="wrapper" style="text-align: center">
+    <h1 class="title" style="color: #000; font-weight: 900; font-family: Varela Round, sans-serif; font-size: 2.75em; text-shadow: 2px 2px 5px yellow">Protect What You Value</h1>
+    <h1 class="title" style="text-decoration: none; font-family: Varela Round, sans-serif; font-weight: 900; font-size: 1.75em; color: #000; text-shadow: 2px 2px 4px yellow">1.844.671.4300</h1>
+    <h1 class="title tap_call" style="display: none"><a href="tel:1-844-671-4300" style="text-decoration: none; color: red; font-family: Montserrat, sans-serif, cursive; font-style: italic; font-weight: 900; font-size: 1.25em"><img src="images/tap-to-call.png" style="width: 200px"></a></h1>
+  </div>
+
+</section>
+
+<div id="content_holder" class="clearfix">
+<section id="content" style="background-color: #fff; border-radius: 15px; padding: 10px; font-family: Varela Round, sans-serif">
+<?php
+ 
+if(isset($_POST['submit'])) { 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "threebrotherswaterproofing@gmail.com";
+ 
+    $email_subject = "Online Contact Form Submission";
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted.";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['last_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['phones'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name']; // required
+ 
+    $last_name = $_POST['last_name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $telephone = $_POST['phones']; // not required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+ 
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+ 
+      
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+ 
+ 
+<h2 style="text-align: center; color: #000; font-weight: bold">Thank you for contacting us. We will be in touch with you very soon. 
+<br><br>
+If you have a need to speak with someone right away, please feel free to call us at 1.844.671.4300</h2>
+ 
+ 
+<?php
+ 
+}
+ 
+?>
+</section>
+</div>
+
+
+
+
+
+
+
+<h1 class="title tap_call" style="display: none"><a href="tel:1-844-671-4300" style="text-decoration: none; color: red; font-family: Montserrat, sans-serif, cursive; font-style: italic; font-weight: 900; font-size: 1.25em"><img src="images/tap-to-call.png" style="width: 200px"></a></h1>
+<!-- footer area -->    
+<footer style="text-align: center">
+  <ul id="footer_list">
+    <li><a href="index.html" style="color: inherit; text-decoration: underline">Home</a></li>
+    <li><a href="waterproofing.html" style="color: inherit; text-decoration: underline">Waterproofing</a></li>
+    <li><a href="mold.html" style="color: inherit; text-decoration: underline">Mold Removal</a></li>
+    <li><a href="cracks.html" style="color: inherit; text-decoration: underline">Crack Repair</a></li>
+    <li><a href="sealing.html" style="color: inherit; text-decoration: underline">Sealing & Painting</a></li>
+    <li><a href="outside.html" style="color: inherit; text-decoration: underline">Outside Drainge</a></li>
+    <li><a href="remodeling.html" style="color: inherit; text-decoration: underline">Remodeling</a></li>
+    <li><a href="plumbing.html" style="color: inherit; text-decoration: underline">Plumbing</a></li>
+    <li><a href="what-we-do.html" style="color: inherit; text-decoration: underline">What We Do</a></li>
+    <li><a href="how-we-do.html" style="color: inherit; text-decoration: underline">How We Do It</a></li>
+    <li><a href="basement-faq.html" style="color: inherit; text-decoration: underline">Basement FAQs</a></li>
+    <li><a href="mold-faq.html" style="color: inherit; text-decoration: underline">Mold FAQs</a></li>
+    <li><a href="resources.html" style="color: inherit; text-decoration: underline">Resources</a></li>
+    <li><a href="wp_gallery.html" style="color: inherit; text-decoration: underline">Waterproofing Photos</a></li>
+    <li><a href="mold_gallery.html" style="color: inherit; text-decoration: underline">Mold Photos</a></li>
+    <li><a href="crack_gallery.html" style="color: inherit; text-decoration: underline">Foundation Photos</a></li>
+    <li><a href="remod_gallery.html" style="color: inherit; text-decoration: underline">Remodeling Photos</a></li>
+    <li><a href="about.html" style="color: inherit; text-decoration: underline">About Us</a></li>
+    <li><a href="affiliations.html" style="color: inherit; text-decoration: underline">Affiliations</a></li>
+    <li><a href="contact.html" style="color: inherit; text-decoration: underline">Contact Us</a></li>
+    <li><a href="coupon.html" style="color: inherit; text-decoration: underline">Coupon</a></li>
+    <li><a href="privacy.html" style="color: inherit; text-decoration: underline">Privacy Policy</a></li>
+    <li><a href="coverage.html" style="color: inherit; text-decoration: underline">Coverage Area</a></li>
+  </ul>
+  <br><br>
+  <div id="colophon" class="clearfix" style="color: #fff; margin-top: -20px">
+      <div style="font-size: 1.9em; margin-bottom: 10px; font-weight: 900">Fully Licensed & Insured!</div>
+      <div>
+        <p style="background: #ffff00; color: #000; font-weight: 600; display: block; width: 240px; margin: 0 auto; font-size: 1.15em; border-radius: 5px; padding: 5px 8px">PA License# PA010948</p>
+      </div>
+      <div>
+        <p style="background: #ffff00; color: #000; font-weight: 600; display: block; width: 240px; margin: 0 auto; font-size: 1.15em; border-radius: 5px; margin-top: 5px; padding: 5px 8px">NJ License# 13VH04721000</p>
+      </div>
+      <div>
+        <p style="background: #ffff00; color: #000; font-weight: 600; display: block; width: 240px; margin: 0 auto; font-size: 1.15em; border-radius: 5px; margin-top: 5px; padding: 5px 8px">DE License# 15-92049-13-000</p>
+      </div>
+  </div>
+
+    <p style="text-align: center; margin-top: 5px; color: #fff"><a href="mailto:3brotherswaterproofing@gmail.com" style="text-decoration: none; color: #fff">3BrothersWaterproofing@gmail.com</a> <span style="font-size: 1.75em">|</span> <a href="tel:1-844-671-4300" style="color: #fff">1.844.671.4300</a></p>
+
+      <div id="bbb">
+        <a href="http://www.bbb.org/washington-dc-eastern-pa/business-reviews/construction-and-remodeling-services/3-brothers-waterproofing-in-philadelphia-pa-80003677/" target="_blank"><img class="bb_icon" src="images/bbb.png" style="width: 270px; height: 70px"></a>
+      </div>
+
+      <div id="social_media">
+        <a href="https://www.facebook.com/3BrothersWaterproofing" target="_blank"><img  class="fb_icon" src="images/fb_icon.png"></a>
+        <a href="https://twitter.com/3BrosWaterproof" target="_blank"><img class="twitter_icon" src="images/twitter_icon.png"></a>
+        <a href="https://plus.google.com/107090787048365487828/about" target="_blank"><img class="google_icon" src="images/google_icon.png"></a>
+        <a href="https://www.linkedin.com/company/tbs-basement-waterproofing" target="_blank"><img class="linkedin_icon" src="images/linkedin_icon.png"></a>
+      </div>
+
+   <p style="text-align: center; margin-top: 5px">&copy; <?php echo date("Y") ?> - <a href="http://www.threebrotherswaterproofing.com" style="text-decoration: none; color: #ffff00">3 Brothers Waterproofing</a></p>
+
+
+</footer><!-- #end footer area -->   
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.9.0.min.js">\x3C/script>')</script>
+
+<script defer src="js/flexslider/jquery.flexslider-min.js"></script>
+
+<!-- fire ups - read this file!  -->   
+<script src="js/main.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/script.js"></script>
+
+<!-- Google Code for Remarketing Tag -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 954213983;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/954213983/?value=0&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
+
+</body>
+</html>
